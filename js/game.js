e6e1d5bf8b8d905d57bd989eb7f178c5c4ed3264
Alex Huynh <alexh3925@gmail.com>
2019-02-20 02:11:02 +0000

    added storyline

--- js/game.js
+++ js/game.js
@@ -6,41 +6,123 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "It's 6:30 in the morning",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "wake up",
+                    nextLevel: "living room",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "continue sleeping",
+                    nextLevel: "bedroom",
                 },
             ]
         },
 
-        cave: {
+        living_room: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You've brushed, showered, ate, and are now chilling in your living room",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "go to school",
+                    nextLevel: "front entrance",
                 },
+                
+                {
+                    text: "stay home",
+                    nextLevel: "bedroom 2"
+                }
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+        front entrance: {
+            message: "You are at the front gate of school",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "go to class",
+                    nextLevel: "classroom"
                 },
+                
+                {
+                    text: "ditch school and go downtown",
+                    nextLevel: "downtown"
+                }
             ]
         },
+        
+        classroom: {
+            message: "You are in class and your teacher gives you a pop quiz",
+            choices: [
+                {
+                    text: "take the quiz",
+                    nextLevel: "hallway"
+                },
+                
+                {
+                    text: "run out and go to downtown",
+                    nextLevel: "downtown"
+                },
+                
+                {
+                    text: "punch the teacher",
+                    nextLevel: "counseling office"
+                }
+            ]
+        },
+        
+        hallway: {
+            message: "Your teacher gave you back the quiz and you failed it",
+            choices: [
+                {
+                    text: "finish the rest of the school day",
+                    nextLevel: "front entrance 2"
+                },
+                
+                {
+                    text: "punch the teacher",
+                    nextLevel: "counseling office"
+                }
+            ]
+        },
+        
+        front_entrance_2: {
+            message: "You just went through all your classes and are now heading out",
+            choices: [
+                {
+                    text: "go home",
+                    nextLevel: "bedroom 2"
+                },
+                
+                {
+                    text: "go to downtown",
+                    nextLevel: "downtown"
+                }
+            ]
+        },
+        
+        bedroom_2: {
+            message: "You just got home",
+            choices: [
+                {
+                    text: "do homework",
+                    nextLevel: "bedroom night"
+                },
+                
+                {
+                    text: "watch Netflix",
+                    nextLevel: "bedroom night"
+                },
+                
+                {
+                    text: "stare at the wall/ceiling",
+                    nextLevel: "bedroom night"
+                }
+            ]
+        },
+        
+        
 
     }
 };
